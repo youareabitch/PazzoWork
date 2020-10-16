647585728bbe45fac32aeb3dd0c728c005658e45
Robert <jimmy98787@gmail.com>
2020-10-16 16:27:11 +0800

    add model to git

---
+++ PazzoApi/PazzoApo.Model/BusinessModel/ProductsBM.cs
@@ -0,0 +1,27 @@
+﻿using PazzoApi.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PazzoApo.Model.BusinessModel
+{
+    public class ProductsBM
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<int> SupplierID { get; set; }
+        public Nullable<int> CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public Nullable<short> UnitsInStock { get; set; }
+        public Nullable<short> UnitsOnOrder { get; set; }
+        public Nullable<short> ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+        //public virtual Categories Categories { get; set; }
+        //public virtual ICollection<Order_Details> Order_Details { get; set; }
+        //public virtual Suppliers Suppliers { get; set; }
+    }
+}
